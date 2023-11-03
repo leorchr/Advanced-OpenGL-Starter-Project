d79c348b1a88a3fcffac11da083238f8016db6fb
L3o <leogravillon@hotmail.fr>
2023-11-03 10:11:02 +0100

    [add] pong ball

--- GLP/SDL-0/SDL-0/Main.cpp
+++ GLP/SDL-0/SDL-0/Main.cpp
@@ -13,41 +13,15 @@ int main(int argc, char* argv[])
 {
 	float updatePosX = 0.0f;
 	float updatePosY = 0.0f;
-	float speedX = 0.01, speedY = 0.01;
-	float maxX = 0.4f;
-	float minY = -0.05f, maxY = 0.5f;
+	float speedX = 0.02, speedY = 0.01;
+	float maxX = 0.1f;
+	float maxY = 0.1f;
 
 	float vertices[] = {
-
-			 0.0f, 0.0f, 0.0f,  1.0f, 0.0f, 0.0f,
-			 0.3f, 0.0f, 0.0f,  0.0f, 1.0f, 0.0f,
-			 0.3f, 0.2f, 0.0f,  0.0f, 0.0f, 1.0f,
-
-			 0.3f, 0.0f, 0.0f,  1.0f, 0.0f, 0.0f,
-			 0.2f, 0.4f, 0.0f,  0.0f, 1.0f, 0.0f,
-			 0.3f, 0.4f, 0.0f,  0.0f, 0.0f, 1.0f,
-
-			 0.3f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
-			 0.2f, 0.4f, 0.0f,  0.0f, 1.0f, 0.0f,
-			 0.3f, 0.4f, 0.0f,  0.0f, 0.0f, 1.0f,
-
-			 0.3f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
-			 0.4f, 0.4f, 0.0f,  0.0f, 1.0f, 0.0f,
-			 0.3f, 0.4f, 0.0f,  0.0f, 0.0f, 1.0f,
-
-			 0.1f, 0.0f, 0.0f,  1.0f, 0.0f, 0.0f,
-			 0.05f, 0.0f, 0.0f,  0.0f, 1.0f, 0.0f,
-			 0.1f,-0.05f, 0.0f,  0.0f, 0.0f, 1.0f,
-
-			 0.25f, 0.0f, 0.0f,  1.0f, 0.0f, 0.0f,
-			 0.2f, 0.0f, 0.0f,  0.0f, 1.0f, 0.0f,
-			 0.25f, -0.05f, 0.0f,  0.0f, 0.0f, 1.0f,
-	};
-
-	float vertices2[] = {
-			-0.5f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
-			0.5f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-			0.0f,  -0.5f, 0.0f,  0.0f, 0.0f, 1.0f
+			0.0f, 0.0f, 0.0f,  1.0f, 0.0f, 0.0f,
+			0.0f, 0.1f, 0.0f,  0.0f, 1.0f, 0.0f,
+			0.1f, 0.0f, 0.0f,  0.0f, 0.0f, 1.0f,
+			0.1f, 0.1f, 0.0f,  0.0f, 0.0f, 1.0f
 	};
 
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
@@ -62,7 +36,7 @@ int main(int argc, char* argv[])
 	//Create a simple window
 
 	int width = 800;
-	int height = 600;
+	int height = 500;
 	unsigned int center = SDL_WINDOWPOS_CENTERED;
 	SDL_Window* Window = SDL_CreateWindow("My window", center, center, width, height, SDL_WINDOW_OPENGL);
 	//SDL_WINDOW_OPENGL is a u32 flag !
@@ -133,54 +107,6 @@ int main(int argc, char* argv[])
 
 	}
 
-	//Create an ID to be given at object generation
-	unsigned int vbo2;
-	unsigned int shaderProgram2;
-	{
-
-		//Pass how many buffers should be created and the reference of the ID to get the value set
-		glGenBuffers(1, &vbo2);
-
-		string vs = LoadShader("Vertex_Shader.shader");
-		const char* vertexShaderSource = vs.c_str();
-		string fs = LoadShader("SimpleFragment.shader");
-		const char* fragmentShaderSource = fs.c_str();
-
-
-		unsigned int vertexShader;
-		vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-
-		//now that we have a vertex shader, let’s put the code text inside
-		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-
-
-		//aaaaand… Compile !
-		glCompileShader(vertexShader);
-
-
-		//Do the same with the fragment shader !
-		unsigned int fragmentShader;
-		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-		glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-		glCompileShader(fragmentShader);
-
-		shaderProgram2 = glCreateProgram();
-
-
-
-		//now attach shaders to use to the program
-		glAttachShader(shaderProgram2, vertexShader);
-		glAttachShader(shaderProgram2, fragmentShader);
-
-		//and link it 
-		glLinkProgram(shaderProgram2);
-
-		//now that the program is complete, we can use it 
-		glUseProgram(shaderProgram2);
-
-	}
-
 	//Create one ID to be given at object generation
 	unsigned int vao;
 	{
@@ -202,34 +128,11 @@ int main(int argc, char* argv[])
 		glEnableVertexAttribArray(1);
 	}
 
-	//Create one ID to be given at object generation
-	unsigned int vao2;
-	{
-		glGenVertexArrays(1, &vao2);
-		glBindVertexArray(vao2);
-
-
-		//Binds the buffer linked to this ID to the vertex array buffer to be rendered. Put 0 instead of vbo to reset the value.
-		glBindBuffer(GL_ARRAY_BUFFER, vbo2);
-
-		//Finally send the vertices array in the array buffer 
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
-
-		// Position attribute
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(0);
-		// Color attribute
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-		glEnableVertexAttribArray(1);
-	}
-
 	//Use depth management
 	glEnable(GL_DEPTH_TEST);
 
 	//0 is our origin, the higher the z, the farther the object
 	glDepthFunc(GL_LESS);
-
-	//OMG WE FINALLY DRAW ! We use the GL_TRIANGLES primitive type
 
 	bool isRunning = true;
 	while (isRunning) {
@@ -252,17 +155,14 @@ int main(int argc, char* argv[])
 		if (updatePosX + maxX >= 1) speedX *= -1;
 		if (updatePosX <= -1) speedX *= -1;
 		if (updatePosY + maxY >= 1) speedY *= -1;
-		if (updatePosY + minY <= -1) speedY *= -1;
+		if (updatePosY <= -1) speedY *= -1;
 
 		int location = glGetUniformLocation(shaderProgram, "updatePos");
+		glUniform2f(location, updatePosX, updatePosY);
+
 		glUseProgram(shaderProgram);
 		glBindVertexArray(vao);
-		glUniform2f(location, updatePosX, updatePosY);
-		glDrawArrays(GL_TRIANGLES, 0,sizeof(vertices)/3);
-
-		glUseProgram(shaderProgram2);
-		glBindVertexArray(vao2);
-		glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / 3);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
 		SDL_GL_SwapWindow(Window); // Swapbuffer
 
